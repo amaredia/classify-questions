bab20480bfbbe91255882211ff599aacb636b5f5
Angel Maredia <angelmaredia@gmail.com>
2017-01-12 11:06:57 -0600

    Added more stop words

--- classify.py
+++ classify.py
@@ -1,9 +1,9 @@
 # gensim modules
-from gensim import utils
 from gensim.models import Word2Vec
-import re
 import numpy as np
 
+stopwords = ['a', 'to', 'and', 'of', 'um', 'mkay', 'okay', 'uh', 'um', 'er', 
+'ah', 'eh', 'oh', 'so','haha', 'mm', 'how', 'whom', 'who', 'what', 'do', 'did']
     
 #Loading model
 print "Loading model"
@@ -29,7 +29,6 @@ def parseRawQFile(fileName):
             line = line.replace("e-reader", "ereader")
             line = line.replace("-", " ")
             line = line.replace("'", "")
-            stopwords = ['a', 'to', 'and', 'of', 'um']
             stripped = line.split(" ")
             stripped  = [word for word in stripped if word not in stopwords]
             
@@ -45,8 +44,7 @@ def parseRawQFile(fileName):
     
 #Parses through file for each turn and writes to file
 def classifyTurns(fileName):
-    stopwords = ['a', 'to', 'and', 'of', 'um', 'mkay', 'okay', 'uh', 'um', 'er', 'ah', 'eh', 'oh', 'so']
-    #clean text using stop words, duplicates
+    #clean text using stop words
     f = open(fileName, 'r')
     lines = filter(None, (line.rstrip() for line in f))
     lines.pop(0)
@@ -54,17 +52,23 @@ def classifyTurns(fileName):
     q_score = convertToDict('q_vect.txt')
     #initialize array of questions with yes or no
     for line in lines:
+        cleaned = list()
         sentence = line.split(",")
         question = sentence[4]
         
         question = question.lower()
-        question = question.replace("-", " ")
+        question = question.replace("-", "")
+        question = question.replace("iceskating", "ice skating")
         question = question.replace("'", "")
         question = question.split(" ")
         question  = [word for word in question if word not in stopwords]
         
+        question.sort()
+        
+        cleaned = [word for word in question if word not in cleaned]
+        
         if len(question) != 0:
-            q_avg = assignVectorAvg(question)
+            q_avg = assignVectorAvg(cleaned)
             closestMatchQ, closestMatchV = min(q_score.items(), key=lambda (_, v): abs(v - q_avg))
             print str(sentence[0]) + " " + str(q_avg) + " " + str(closestMatchQ) + " " + str(closestMatchV)
     
@@ -81,8 +85,10 @@ def assignVectorAvg(phrase):
             wordCount = wordCount + 1
         except KeyError:
             print word
+    if wordCount == 0:
+        return 0
+    
     avg = sum/wordCount
-        
     return np.average(avg)
 
 
@@ -100,7 +106,8 @@ def convertToDict(fileName):
         
 
 def main():
-    classifyTurns("labeled/p324p325-part2_ch1.csv")
+    #parseRawQFile("questions.csv")
+    classifyTurns("labeled/p364p365-part2_ch1.csv")
 
 if __name__ == "__main__":
     main()
